f5c8d36bfb6bcbd751b2c99e802d62276f1af9d1
Karunesh Ranjan <karunesh029@gmail.com>
2024-08-30 17:33:20 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css"
+function App() {
+
+  return (
+    <div className="counter">
+     <div className="card">
+     <div className="h1">
+     <h1>Counter</h1>
+     </div>
+     <div className="botton">
+     <button>Increment</button>
+     <button>Decrement</button>
+     </div>
+     </div>
+    </div>
+  )
+}
+
+export default App
